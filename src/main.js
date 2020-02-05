6b77e0e41ae836a4e57916d01d0b643e5b511f13
umairabid <umairac90@gmail.com>
2020-02-05 15:46:20 +0500

    adding light weight pdf

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ function CreatePDFfromHTML() {
   }
 
   html2canvas($(".html-content")[0], options).then(function (canvas) {
-      var imgData = canvas.toDataURL("image/png", 1.0);
+      var imgData = canvas.toDataURL("image/jpeg", 1.0);
       // const div = document.createElement('div');
       // div.innerHTML = '<img src="'+ imgData +'" />';
       // document.body.appendChild(div);
       var pdf = new jsPDF('p', 'pt', [width, height]);
-      pdf.addImage(imgData, 'PNG', -10, 0, width, height);
+      pdf.addImage(imgData, 'JPG', -10, 0, width, height);
       pdf.save("Your_PDF_Name.pdf");
   });
 }
