fb614cdfb10fb9e6a5943f51c56a24881e1d4861
Umair Abid <umairac90@gmail.com>
2021-10-04 00:01:49 -0700

    chanegd content

--- content/index.js
+++ content/index.js
@@ -2,7 +2,7 @@ module.exports = {
   contactInfo: [
     {
       name: 'Phone',
-      value: '+923214234193'
+      value: '+1 236 334 1919'
     },
     {
       name: 'E-mail',
@@ -19,6 +19,10 @@ module.exports = {
     {
       name: 'StackOverflow',
       value: 'https://stackoverflow.com/users/975932/umair-abid'
+    },
+    {
+      name: 'LeetCode',
+      value: 'https://leetcode.com/umairabid/'
     },
     {
       name: 'HackerRank',
@@ -56,39 +60,48 @@ module.exports = {
     },
     {
       name: 'Mysql',
-      level: 'eight'
+      level: 'six'
     },
     {
       name: 'Postgres',
-      level: 'six'
+      level: 'eight'
     },
   ],
   summary: "Full stack developer with 6+ years of experience in developing web applications. Capable of designing and implementing architecture of both web clients and APIs for a small to medium level application. An avid learner and challenge oriented engineer, motivated by solving business problem and customer success. Advocates simplicity, strong typing and functional approach.",
   experiences: [
     {
-      from: "2019-06",
-      to: "present",
+      from: "2020-05",
+      to: "Present",
       title: "Senior Software Engineer",
-      company: "KeepTruckin",
+      company: "Rise People Inc.",
       summary: [
-        "Currently working in fleet view team where we are using google maps, pusher and service workers to provide real time location and real time information updates of fleet to fleet managers",
-        "We also used web workers to generate custom map markers to reduce map loading time for 8 seconds to 2 seconds",
-        "Redesigned the incidents feature which fixed all the sync issues of different incident's videos playing together by creating a service to provide single source of truth.",
-        "Refactored graph component of incidents feature by using interpolated data to give accurate speed and time information at any given point of time in video",
-        "Redesigned the layout to allow subsequent components to react to changes in state of application by extensively using rxjs.",
-        "Introduced and planned hackathons for better team collaboration and growth"
+        'Refactored benefits admin dashboard to build employee summaries by crunching data from 3 different services to bring down max load time from 15 seconds to 650ms',
+        'Refactored entire volume calculation service to cover incorporate complex business rules and brought down support requests by 80%',
+        'Contributed to org wide event sourcing architecture to integrate separate channels for temportal and linear data channels, without disrupting existing services',
+        'Acted as devops deputy for my team to deal with infra level blockers'
       ]
     },
     {
-      from: "2018-11",
-      to: "2019-05",
+      from: "2019-06",
+      to: "2020-05",
+      title: "Senior Software Engineer",
+      company: "KeepTruckin",
+      summary: [
+        'Designed architecture for fleet manager dashboard, to provide real time position and movement of fleet with no noticable lag and maximum accuracy',
+        'Redesigned app container with event driven system to allow subsequent components to capture global app state changes',
+        'Refactored vehicle monitoring and incidents dashboard to build accurate visualizations',
+        'Introduced and planned hackathons for better team collaboration and growth'
+      ]
+    },
+    {
+      from: "2018-10",
+      to: "2019-06",
       title: "Senior Software Engineer",
       company: "Exponento",
       summary: [
-        "Redesigned and developed the automations sequences which brought down the CPU usage from 80% (due to large number of MySql connections) to 20%.",
-        "Designed and developed value added feature, appointment scheduler (like calendly) for the product, which was integrated with automations and google calendar.",
+        "Redesigned lead pipeline automated tasks to reduce the CPU consumption from 80% max usage to 20% max usage of workers",
+        "Designed and developed value added feature, appointment scheduler (like calendly) for the product, which gave an edge over competitors and won new customers.",
         "Customized and integrated email builder and built SMS builder to create marketing templates",
-        "Designed the solution to enable automations engine to have multiple leads in multiple stages where previously it only allowed one lead in only one automation at a time"
       ]
     },
     {
@@ -97,10 +110,11 @@ module.exports = {
       title: "Senior Software Engineer",
       company: "Tkxel",
       summary: [
-        "Worked and led the development of people analytics tool, we mostly worked on deducing aggregate based data (for clustering) and then building visualizations on top of it. We helped our client won funding from government and private investors. Canva is one of their clients.",
-        "Worked on a replenishment based software built on top of microservices architecture. I mostly worked on maintaining and adding new features in user’s calendar of orders",
-        "With help of team, I took projects from discovery phase and managed SDLC, client communications, team growth and client success throughout the project's life cycle",
-        "I participated in discovery calls with our sales team. I also won half a dozen projects through technical interviews and assignments. Some of the projects I won expanded upto team of 6 developers and QA."
+        "Led development of people analytical tool involving cruching personal personality traits to calculate organization wide trends. Helped our client to realize their vision and secure funding",
+        "Managed SDLC of 7 projects and led them to successful completion",
+        "Worked with sales team in pre sales phase to win projects for Tkxel",
+        "Enabled junior developers to take on ownership on projects and communication",
+        "Actively contributed to Javascript and Ruby on Rails knowledge groups to lift org wide skills level"
       ]
     },
   ],
